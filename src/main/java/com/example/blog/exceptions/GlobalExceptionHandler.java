0c6129351261149fa914cbd79bd138b606a77586
nandinisahu407 <nandinisahu407@gmail.com>
2024-11-23 20:46:06 +0530

    custom exception handler added

---
+++ src/main/java/com/example/blog/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.blog.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        Map<String,String> response=new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error)->{
+            String fieldName=((FieldError)error).getField();
+            String msg=error.getDefaultMessage();
+            response.put(fieldName,msg);
+        });
+        return new ResponseEntity<Map<String,String>>(response, HttpStatus.BAD_REQUEST);
+    }
+
+}
