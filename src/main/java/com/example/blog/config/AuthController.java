552227aa51e1cf3d5365775a6de74290e56e5976
nandinisahu407 <nandinisahu407@gmail.com>
2024-11-28 23:59:53 +0530

    fix: jwt issue

--- src/main/java/com/example/blog/config/AuthController.java
+++ src/main/java/com/example/blog/config/AuthController.java
@@ -1,8 +1,12 @@
 package com.example.blog.config;
 
+import com.example.blog.entity.User;
+import com.example.blog.exceptions.ResourceNotFoundException;
 import com.example.blog.model.JwtRequest;
 import com.example.blog.model.JwtResponse;
+import com.example.blog.repository.UserRepository;
 import com.example.blog.security.JwtHelper;
+import com.example.blog.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +17,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,7 +28,10 @@ public class AuthController {
 
     @Autowired
     private AuthenticationManager manager;
-
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private JwtHelper helper;
@@ -33,6 +41,11 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest request) {
         logger.info("Reached in controller => ");
+
+        User user=this.userRepository.findByEmail(request.getEmail()).orElseThrow(()-> new ResourceNotFoundException("User","email: "+request.getEmail(),0));
+        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())){
+            return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
+        }
 
         UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
         String token = this.helper.generateToken(userDetails);
